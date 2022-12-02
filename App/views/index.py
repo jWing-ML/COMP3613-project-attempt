72d5ded972908a1f4cec14143593a2e71e6d09c2
jonathonwingson <jonathonwingson@gmail.com>
2022-12-02 04:27:03 +0000

    urls

--- App/views/index.py
+++ App/views/index.py
@@ -20,7 +20,7 @@ index_views = Blueprint('index_views', __name__, template_folder='../templates')
 def index_page():
     
     create_user("bob", "password")
-    create_image("1", "https://th.bing.com/th/id/OIP.-A2Z_EY0ick_wctAjgUqZQHaET?w=318&h=184&c=7&r=0&o=5&pid=1.7")
+    create_image("1", "https://th.bing.com/th/id/OIP.REFN5HplO447i4UvNLizlwHaFn?pid=ImgDet&rs=1")
 
     create_user("betty", "password")
     create_image("2", "https://th.bing.com/th/id/OIP.9nzbJlzKhuMzTLUOpObDDQHaHC?w=204&h=194&c=7&r=0&o=5&pid=1.7")
@@ -28,8 +28,8 @@ def index_page():
     create_user("brian", "password")
     create_image("3", "https://i0.wp.com/www.urbandognyc.com/wp-content/uploads/2018/10/brian-family-guy-dog-brought-back-to-life.jpg?fit=1500%2C1500&ssl=1")
     
-    create_user("becky", "password")
-    create_image("4", "https://th.bing.com/th/id/OIP.of0QWVWaAAm1hZcefnxmhwAAAA?pid=ImgDet&rs=1")
+    create_user("bean", "password")
+    create_image("4", "https://th.bing.com/th/id/OIP.9vz9tEkrPSk41XpcOQWfpAHaEK?pid=ImgDet&rs=1")
     
     
    
